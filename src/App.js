99a73a7a1acd9388d51d7990e9cce3f3631593fe
claceotto <clatimponi@gmail.com>
2023-12-12 14:04:59 +0000

    Functional log in completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import './App.css';
-import CheckUser from './CheckUser'
+import { useState } from 'react';
+import LogOn from './LogOn';
+import Welcome from './Welcome';
 
 function App() {
 
+  const [user, setUser] = useState(false)
+
+  function handleLogOff() {
+    setUser(false)
+  }
+
+  function handleLogIn(inputName, inputPassword) {
+
+    const userIDs = [
+      {
+        Name: 'Clarisse',
+        Password: 'Ceotto'
+      },
+      {
+        Name: 'Felipe',
+        Password: 'Gagno'
+      }
+    ]
+    const idFound = userIDs.find((id) => { return inputName == id.Name && inputPassword == id.Password });
+
+    setUser(idFound !== undefined)
+    return idFound !== undefined
+  }
+
   return (
-    <CheckUser>
-    </CheckUser>
-  );
+    < div >
+      {user ? (
+        <Welcome
+          logOff={handleLogOff} />
+      ) : (
+        <LogOn
+          logIn={handleLogIn} />
+      )}
+    </div >
+  )
 }
+
 
 export default App;
--- src/LogOn.js
+++ src/LogOn.js
@@ -1,19 +1,40 @@
+import { useState } from "react"
+
 function LogOn({ logIn }) {
+    const [inputName, setInputName] = useState('')
+    const [inputPassword, setInputPassword] = useState('')
+    const [showError, setShowError] = useState(false)
+    function handleLoginClick() {
+        const isLoginSuccessful = logIn(inputName, inputPassword)
+        setShowError(!isLoginSuccessful)
+    }
+
     return (
         <div>
             <h1>You need to log on</h1>
             <form>
+                {showError ? (<h2 className="error">User or Password invalid</h2>) : null}
                 <label>
                     Name:
-                    <input type='text' name='name' />
+                    <input
+                        type='text'
+                        name='name'
+                        onChange={event => {
+                            setInputName(event.target.value)
+                        }} />
                 </label>
                 <br />
                 <label>
                     Password:
-                    <input type='password' name='password' />
+                    <input
+                        type='password'
+                        name='password'
+                        onChange={event => {
+                            setInputPassword(event.target.value)
+                        }} />
                 </label>
             </form>
-            <button onClick={logIn}>Log in</button>
+            <button onClick={handleLoginClick}>Log in</button>
         </div>)
 }
 
